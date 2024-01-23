fa89e938f1f270104a2b92bcd6088e588a894fc2
R894 <kamil.khalaileh@gmail.com>
2024-01-23 14:05:35 +0200

    feat: add bug.go domain

--- server/internal/bug/domain/bug.go
+++ server/internal/bug/domain/bug.go
@@ -1,1 +1,56 @@
 package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type BugStatus string
+
+const (
+	BugStatusOpen    BugStatus = "Open"
+	BugStatusClosed  BugStatus = "Closed"
+	BugStatusPending BugStatus = "Pending"
+)
+
+type Bug struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      BugStatus `json:"status"`
+	Assignee    string    `json:"asignee"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+func NewBug(title, description string) *Bug {
+	return &Bug{
+		ID:          generateUniqueID(),
+		Title:       title,
+		Description: description,
+		Status:      BugStatusOpen,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+}
+
+func (b *Bug) UpdateDetails(title, description string) {
+	b.Title = title
+	b.Description = description
+	b.UpdatedAt = time.Now()
+}
+
+func (b *Bug) ChangeStatus(status BugStatus) {
+	b.Status = status
+	b.UpdatedAt = time.Now()
+}
+
+func (b *Bug) AssignTo(assignee string) {
+	b.Assignee = assignee
+	b.UpdatedAt = time.Now()
+}
+
+func generateUniqueID() string {
+	return uuid.New().String()
+}
