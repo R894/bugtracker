5dda73d76c948dcc8f5ee016548763e5beeab94b
R894 <kamil.khalaileh@gmail.com>
2024-01-28 00:13:30 +0200

    feat: add bug and user routes

--- server/internal/web/router/router.go
+++ server/internal/web/router/router.go
@@ -1,12 +1,36 @@
 package router
 
 import (
+	"bugtracker/internal/database"
+	"bugtracker/internal/web/handler"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func SetupRouter() *chi.Mux {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	return router
+	db, err := database.NewPostgresDB()
+	if err != nil {
+		panic(err)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	bugHandler := handler.NewBugHandler(db)
+	userHandler := handler.NewUserHandler(db)
+
+	r.Route("/bugs", func(r chi.Router) {
+		r.Get("/", bugHandler.GetBugs)
+		r.Post("/", bugHandler.CreateNewBug)
+		r.Get("/{bugId}", bugHandler.GetBugByID)
+	})
+
+	r.Route("/users", func(r chi.Router) {
+		r.Get("/", userHandler.CreateNewUser)
+		r.Post("/", userHandler.CreateNewUser)
+		r.Get("/{userId}", userHandler.GetUserById)
+	})
+
+	return r
 }
--- server/internal/web/server.go
+++ server/internal/web/server.go
@@ -2,10 +2,19 @@ package web
 
 import (
 	"bugtracker/internal/web/router"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func StartServer() {
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "3333"
+	}
+
 	r := router.SetupRouter()
-	http.ListenAndServe(":3333", r)
+	address := fmt.Sprintf(":%s", port)
+
+	http.ListenAndServe(address, r)
 }
