12185d81ed9cde3c91bd7430a975f5400edb3c96
R894 <kamil.khalaileh@gmail.com>
2024-02-04 00:49:06 +0200

    chore: modify repository interface

--- server/internal/domain/comment/repository/repository.go
+++ server/internal/domain/comment/repository/repository.go
@@ -1,9 +1,13 @@
 package repository
 
-import "context"
+import (
+	"bugtracker/internal/domain/comment/aggregate"
+	"context"
+)
 
 type CommentRepository interface {
-	CreateComment(ctx context.Context, bugId, content string) error
+	SaveComment(ctx context.Context, comment aggregate.Comment) error
 	UpdateComment(ctx context.Context, commentId, content string) error
 	DeleteComment(ctx context.Context, commentId string) error
+	GetCommentsByBugId(ctx context.Context, bugId string) ([]aggregate.Comment, error)
 }
