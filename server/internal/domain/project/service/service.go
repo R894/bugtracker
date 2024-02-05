8d0949033439a169bf60b7b99e9a2dff76b57dfc
R894 <kamil.khalaileh@gmail.com>
2024-02-05 02:30:22 +0200

    feat: add project service methods

--- server/internal/domain/project/service/service.go
+++ server/internal/domain/project/service/service.go
@@ -3,22 +3,32 @@ package service
 import (
 	"bugtracker/internal/domain/project/aggregate"
 	"bugtracker/internal/domain/project/repository"
+	"context"
+	"database/sql"
 )
 
 type ProjectService struct {
 	repo repository.Repository
 }
 
-func NewProjectService(projectRepo repository.Repository) *ProjectService {
+func NewProjectService(db *sql.DB) *ProjectService {
 	return &ProjectService{
-		repo: projectRepo,
+		repo: repository.NewPostgresRepository(db),
 	}
 }
 
-func (s *ProjectService) CreateProject(name, description, ownerId string) (*aggregate.Project, error) {
-	project, err := s.repo.Save(name, description, ownerId)
+func (s *ProjectService) CreateProject(ctx context.Context, name, description, ownerId string) (*aggregate.Project, error) {
+	project, err := s.repo.Save(ctx, name, description, ownerId)
 	if err != nil {
 		return nil, err
 	}
 	return project, nil
+}
+
+func (s *ProjectService) GetProjectByID(ctx context.Context, projectId string) (*aggregate.Project, error) {
+	return s.repo.GetById(ctx, projectId)
+}
+
+func (s *ProjectService) GetProjects(ctx context.Context) ([]aggregate.Project, error) {
+	return s.repo.GetProjects(ctx)
 }
