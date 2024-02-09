1204f9a77d02d2d0edf20ad65544f76b6f4a7a94
R894 <kamil.khalaileh@gmail.com>
2024-02-09 18:58:51 +0200

    chore: add test db function

--- server/internal/database/postgresql.go
+++ server/internal/database/postgresql.go
@@ -31,6 +31,25 @@ func NewPostgresDB() (*PostgresDB, error) {
 	}, nil
 }
 
+func NewPostgresTestDB() (*PostgresDB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := "test"
+
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostgresDB{
+		Db: db,
+	}, nil
+}
+
 func (p *PostgresDB) Close() {
 	p.Db.Close()
 }
