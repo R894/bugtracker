4845f92a149eecd5b8a53488d12fb6911dbef835
R894 <kamil.khalaileh@gmail.com>
2024-02-17 00:15:28 +0200

    chore: lint files

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,6 +1,18 @@
-import LoginForm from "@/components/LoginForm";
-import { Container } from "@mui/material";
+import LoginForm from '@/components/LoginForm/LoginForm'
+import NavBar from '@/components/NavBar/NavBar'
+import Head from 'next/head'
 
-export default function login() {
-  return <Container maxWidth="sm"><LoginForm /></Container>
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <NavBar />
+        <LoginForm />
+      </main>
+    </>
+  )
 }
