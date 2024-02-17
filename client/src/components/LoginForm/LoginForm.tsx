60d8c8f8c6b70a1a9211730ece063630190cf8ab
R894 <kamil.khalaileh@gmail.com>
2024-02-18 00:11:52 +0200

    refactor: LoginForm.tsx to use Stack

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,13 +1,12 @@
-import { Button, Container, TextField } from '@mui/material'
-import styles from './loginform.module.scss'
+import { Button, Container, Stack, TextField } from '@mui/material'
 const LoginForm = () => {
   return (
     <Container maxWidth="sm">
-      <div className={styles['login-form']}>
+      <Stack spacing={1}>
         <TextField id="standard-basic" label="Email" variant="standard" />
         <TextField id="standard-basic" label="Password" variant="standard" />
         <Button variant="contained">Login</Button>
-      </div>
+      </Stack>
     </Container>
   )
 }
