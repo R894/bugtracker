5c1f454a98086d67141cbff99c452cd695ab9de0
R894 <kamil.khalaileh@gmail.com>
2024-02-19 01:35:08 +0200

    feat: add FeaturesDisplay.tsx

---
+++ client/src/components/FeaturesDisplay/FeaturesDisplay.tsx
@@ -0,0 +1,49 @@
+import { Apartment } from '@mui/icons-material'
+import { Card, Stack, Typography } from '@mui/material'
+
+const Feature = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Card
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '16px'
+      }}
+    >
+      {children}
+    </Card>
+  )
+}
+
+const FeaturesDisplay = () => {
+  return (
+    <>
+      <Stack direction={'row'} gap={3}>
+        <Feature>
+          <Apartment fontSize="large" />
+          <Typography variant='h5'>Feature 1</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+        </Feature>
+        <Feature>
+          <Apartment fontSize="large" />
+          <Typography variant='h5'>Feature 2</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+        </Feature>
+        <Feature>
+          <Apartment fontSize="large" />
+          <Typography variant='h5'>Feature 3</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+          <Typography>Write description here</Typography>
+        </Feature>
+      </Stack>
+    </>
+  )
+}
+
+export default FeaturesDisplay
