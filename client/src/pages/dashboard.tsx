4a80fdf691ccea2a4151734c54482ae1ca724e28
R894 <kamil.khalaileh@gmail.com>
2024-02-20 00:44:32 +0200

    feat: add dashboard page

---
+++ client/src/pages/dashboard.tsx
@@ -0,0 +1,22 @@
+import DashboardDisplay from '@/components/DashboardDisplay/Dashboard'
+import Footer from '@/components/Footer/Footer'
+import NavBar from '@/components/NavBar/NavBar'
+import Head from 'next/head'
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <NavBar />
+        <div className="layout">
+          <DashboardDisplay />
+        </div>
+        <Footer />
+      </main>
+    </>
+  )
+}
