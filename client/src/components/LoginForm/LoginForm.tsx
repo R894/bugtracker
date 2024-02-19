e1d2ded147daa94e542d1d3cff52cadb0d142877
R894 <kamil.khalaileh@gmail.com>
2024-02-20 00:43:52 +0200

    style: modify LoginForm

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,8 +1,9 @@
-import { Button, Container, Stack, TextField } from '@mui/material'
+import { Button, Container, Stack, TextField, Typography } from '@mui/material'
 const LoginForm = () => {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" sx={{minWidth:'320px', padding:'12px'}}>
       <Stack spacing={1}>
+        <Typography variant='h5' sx={{textAlign:'center'}}>Login</Typography>
         <TextField id="standard-basic" label="Email" variant="standard" />
         <TextField id="standard-basic" label="Password" variant="standard" />
         <Button variant="contained">Login</Button>
