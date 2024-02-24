2886da497e26d00522cf8e705d7d6b02bb69d7af
R894 <kamil.khalaileh@gmail.com>
2024-02-24 03:27:50 +0200

    feat: add registerUser to usercontext

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,10 +1,12 @@
-import { LoginRequest } from '@/api/models/user'
-import { postLogin } from '@/api/userService'
-import React, { useState } from 'react'
+import { LoginRequest, RegisterRequest } from '@/api/models/user'
+import { createUser, postLogin } from '@/api/userService'
+import React, { useEffect, useState } from 'react'
 
 export type UserContextType = {
   token: string
+  isLoggedIn: boolean
   loginUser: typeof postLogin
+  registerUser: typeof createUser
   logoutUser: () => void
 }
 
@@ -14,22 +16,48 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [token, setToken] = useState('')
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    const t = localStorage.getItem('token')
+    if (t && t != '') {
+      setToken(t)
+      setIsLoggedIn(true)
+    }
+  }, [])
 
   const loginUser = async (loginRequest: LoginRequest) => {
+    setIsLoggedIn(false)
     const response = await postLogin(loginRequest)
+    if (response.error) {
+      return response
+    }
+    setIsLoggedIn(true)
     const currentToken = response.token
     setToken(currentToken)
     localStorage.setItem('token', currentToken)
     return response
   }
 
+  const registerUser = async (registerRequest: RegisterRequest) => {
+    const response = await createUser(registerRequest)
+    if (response.error) {
+      return response
+    }
+
+    return response
+  }
+
   const logoutUser = () => {
+    setIsLoggedIn(false)
     setToken('')
     localStorage.removeItem('token')
   }
 
   return (
-    <UserContext.Provider value={{ loginUser, logoutUser, token }}>
+    <UserContext.Provider
+      value={{ loginUser, registerUser, isLoggedIn, logoutUser, token }}
+    >
       {children}
     </UserContext.Provider>
   )
