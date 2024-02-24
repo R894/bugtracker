94e14437120cb0393dee76367affaccc68d5ba90
R894 <kamil.khalaileh@gmail.com>
2024-02-24 03:32:34 +0200

    feat: add state check to NavBar login button

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,3 +1,4 @@
+import { UserContext, UserContextType } from '@/context/UserContext'
 import { BugReport } from '@mui/icons-material'
 import {
   AppBar,
@@ -8,9 +9,11 @@ import {
   Typography,
 } from '@mui/material'
 import { useRouter } from 'next/router'
+import { useContext } from 'react'
 
 const NavBar = () => {
   const router = useRouter()
+  const { logoutUser, isLoggedIn } = useContext(UserContext) as UserContextType
   return (
     <>
       <AppBar position="static">
@@ -33,9 +36,15 @@ const NavBar = () => {
             <Button color="inherit" onClick={() => router.push('/dashboard')}>
               Dashboard
             </Button>
-            <Button color="inherit" onClick={() => router.push('/login')}>
-              Login
-            </Button>
+            {isLoggedIn ? (
+              <Button color="inherit" onClick={() => logoutUser()}>
+                Logout
+              </Button>
+            ) : (
+              <Button color="inherit" onClick={() => router.push('/login')}>
+                Login
+              </Button>
+            )}
           </Stack>
         </Toolbar>
       </AppBar>
