844e82346ebda349cb9c56f2bf9bd46aa174fdb9
R894 <kamil.khalaileh@gmail.com>
2024-02-25 12:27:55 +0200

    feat: add user info to UserContext

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -1,9 +1,10 @@
-import { LoginRequest, RegisterRequest } from '@/api/models/user'
+import { LoginRequest, RegisterRequest, User } from '@/api/models/user'
 import { createUser, postLogin } from '@/api/userService'
 import React, { useEffect, useState } from 'react'
 
 export type UserContextType = {
   token: string
+  user: User | null
   isLoggedIn: boolean
   loginUser: typeof postLogin
   registerUser: typeof createUser
@@ -16,6 +17,7 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [token, setToken] = useState('')
+  const [user, setUser] = useState<User | null>(null)
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   useEffect(() => {
@@ -26,6 +28,10 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
     }
   }, [])
 
+  useEffect(() => {
+    console.log(user?.projects)
+  }, [user])
+
   const loginUser = async (loginRequest: LoginRequest) => {
     setIsLoggedIn(false)
     const response = await postLogin(loginRequest)
@@ -34,7 +40,9 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
     }
     setIsLoggedIn(true)
     const currentToken = response.token
+    const currentUser = response.user
     setToken(currentToken)
+    setUser(currentUser)
     localStorage.setItem('token', currentToken)
     return response
   }
@@ -56,7 +64,7 @@ const UserProvider: React.FC<{ children: React.ReactNode }> = ({
 
   return (
     <UserContext.Provider
-      value={{ loginUser, registerUser, isLoggedIn, logoutUser, token }}
+      value={{ loginUser, user, registerUser, isLoggedIn, logoutUser, token }}
     >
       {children}
     </UserContext.Provider>
