185b285c1417d8760e44cd3a3c849ee2ab3ccbb4
R894 <kamil.khalaileh@gmail.com>
2024-02-26 12:01:46 +0200

    feat: add getProjectsByUsername

--- client/src/api/projectService.ts
+++ client/src/api/projectService.ts
@@ -1,5 +1,10 @@
-import { post } from "./utils"
+import { get, post } from './utils'
 
 export const createProject = async (body: object) => {
   await post('projects', body)
+}
+
+export const getProjectsByUsername = async (userId: string, token: string) => {
+  const projects = await get(`projects/${userId}`, token)
+  return projects
 }
