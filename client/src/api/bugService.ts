949cca1f84466f00461d7f7ee3d8badaf34671d6
R894 <kamil.khalaileh@gmail.com>
2024-02-29 00:41:55 +0200

    refactor: add CreateBugRequest interface

--- client/src/api/bugService.ts
+++ client/src/api/bugService.ts
@@ -1,12 +1,12 @@
-import { Bug } from "./models/bug"
+import { Bug, CreateBugRequest } from "./models/bug"
 import { get, post } from "./utils"
 
 export const getBugs = async (token: string): Promise<Bug[]> => {
     return get('bugs', token)
 }
 
-export const createBug = async (bugData: object) => {
-    return post('bugs', bugData)
+export const createBug = async (bugData: CreateBugRequest, token: string) => {
+    return post('bugs', bugData, token)
 }
 
 export const getBugById = async (bugId: string) => {
--- client/src/api/models/bug.ts
+++ client/src/api/models/bug.ts
@@ -9,3 +9,9 @@ export interface Bug {
   createdAt: Date
   updatedAt: Date
 }
+
+export interface CreateBugRequest {
+  title: string
+  description: string
+  projectId: string
+}
