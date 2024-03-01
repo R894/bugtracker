60d9cc64f71b1192b56898e65a415ac168048709
R894 <kamil.khalaileh@gmail.com>
2024-03-02 00:16:52 +0200

    chore: add Roboto font

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import UserProvider from '@/context/UserContext'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <div className={roboto.className}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </div>
   )
 }
