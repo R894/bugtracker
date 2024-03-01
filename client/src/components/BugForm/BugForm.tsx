61e5f4aa578dd6fe474ee6f78bb70575af46510e
R894 <kamil.khalaileh@gmail.com>
2024-03-02 00:16:38 +0200

    style: modify bug form

--- client/src/components/BugForm/BugForm.tsx
+++ client/src/components/BugForm/BugForm.tsx
@@ -1,7 +1,8 @@
 import { getBugById } from '@/api/bugService'
 import { Bug } from '@/api/models/bug'
 import { useUserContext } from '@/context/UserContext'
-import { Box, Card, Stack, Typography } from '@mui/material'
+import { Subject } from '@mui/icons-material'
+import { Box, Card, Divider, Stack, Typography } from '@mui/material'
 import moment from 'moment'
 import { useEffect, useState } from 'react'
 
@@ -18,26 +19,43 @@ const BugDisplay = ({ bugId }: { bugId: string }) => {
 
   return (
     <>
-      <Box>
-        <Stack>
-          <Typography variant="h6">{bug && bug.title}</Typography>
-          <Typography>
-            Opened at {bug && moment(bug.createdAt).format('MMM Do YY')}
-          </Typography>
+      <Card
+        variant="outlined"
+        sx={{
+          minWidth: '640px',
+          minHeight: '72px',
+          marginTop: '12px',
+        }}
+      >
+        <Stack padding={1}>
+          <Stack
+            direction={'row'}
+            gap={1}
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            <Subject />
+            <Typography variant="h5">
+              {bug && bug.title.toUpperCase()}
+            </Typography>
+          </Stack>
+          <Box paddingTop={1} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Typography variant="subtitle2">
+              Opened at {bug && moment(bug.createdAt).format('MMM Do YY')}
+            </Typography>
+            <Typography variant="subtitle2">
+              Status: {bug && bug.status}
+            </Typography>
+            <Typography variant="subtitle2">
+              Assigned to: {bug && bug.asignee}
+            </Typography>
+          </Box>
         </Stack>
-        <Card
-          variant="outlined"
-          sx={{
-            minWidth: '640px',
-            minHeight: '320px',
-            padding: '12px',
-            marginTop: '12px',
-          }}
-        >
-          <Typography>{bug && bug.description}</Typography>
+        <Divider />
+        <Box padding={1}>
+          <Typography variant="body1">{bug && bug.description}</Typography>
           <Typography>{bug && bug.asignee}</Typography>
-        </Card>
-      </Box>
+        </Box>
+      </Card>
     </>
   )
 }
