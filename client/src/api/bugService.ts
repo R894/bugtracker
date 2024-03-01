60f902bd18f2cf56ac90b37d551408bcb847d753
R894 <kamil.khalaileh@gmail.com>
2024-03-01 02:17:46 +0200

    fix: missing token params

--- client/src/api/bugService.ts
+++ client/src/api/bugService.ts
@@ -2,17 +2,17 @@ import { Bug, CreateBugRequest } from "./models/bug"
 import { get, post } from "./utils"
 
 export const getBugs = async (token: string): Promise<Bug[]> => {
-    return get('bugs', token)
+    return get('bugs', token) as Promise<Bug[]>
 }
 
 export const createBug = async (bugData: CreateBugRequest, token: string) => {
     return post('bugs', bugData, token)
 }
 
-export const getBugById = async (bugId: string) => {
-    return get(`bugs/${bugId}`)
+export const getBugById = async (bugId: string, token: string) => {
+    return get(`bugs/${bugId}`, token) as Promise<Bug>
 }
 
 export const getBugsByProjectId = async (projectId: string, token: string) => {
-    return get(`bugs/projects/${projectId}`, token)
+    return get(`bugs/projects/${projectId}`, token) as Promise<Bug[]>
 }
