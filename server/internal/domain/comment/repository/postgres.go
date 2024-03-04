5146136562a32612e5238ce2d59ecf81472e0bff
R894 <kamil.khalaileh@gmail.com>
2024-03-04 02:39:03 +0200

    fix: comment query using wrong column names

--- server/internal/domain/comment/repository/postgres.go
+++ server/internal/domain/comment/repository/postgres.go
@@ -19,7 +19,7 @@ func NewPostgresCommentRepository(db *sql.DB) *PostgresCommentRepository {
 }
 
 func (c *PostgresCommentRepository) SaveComment(ctx context.Context, comment aggregate.Comment) error {
-	query := sq.Insert("comments").Columns("id", "bugId", "content", "createdAt", "updatedAt").
+	query := sq.Insert("comments").Columns("id", "bug_id", "content", "created_at", "updated_at").
 		Values(comment.ID, comment.BugId, comment.Content, comment.CreatedAt, comment.UpdatedAt).PlaceholderFormat(sq.Dollar).RunWith(c.db)
 
 	_, err := query.ExecContext(ctx)
@@ -64,7 +64,9 @@ func (c *PostgresCommentRepository) GetCommentsByBugId(ctx context.Context, bugI
 	var comments []aggregate.Comment
 	for rows.Next() {
 		var comment aggregate.Comment
-		rows.Scan(&comment.ID, &comment.BugId, &comment.Content, &comment.CreatedAt, &comment.UpdatedAt)
+		if err := rows.Scan(&comment.ID, &comment.BugId, &comment.Content, &comment.CreatedAt, &comment.UpdatedAt); err != nil {
+			return nil, err
+		}
 		comments = append(comments, comment)
 	}
 
