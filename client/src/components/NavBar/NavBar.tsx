93a96f7658df09f213c0b0c630b7e025aa3278b6
R894 <kamil.khalaileh@gmail.com>
2024-03-07 02:13:09 +0200

    feat: add burger menu

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,21 +1,70 @@
 import { UserContext, UserContextType } from '@/context/UserContext'
 import { BugReport } from '@mui/icons-material'
+import { useTheme } from '@mui/material/styles'
 import {
   AppBar,
+  Box,
   Button,
+  Drawer,
   IconButton,
   Stack,
   Toolbar,
   Typography,
+  useMediaQuery,
 } from '@mui/material'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
 
 const NavBar = () => {
   const router = useRouter()
+  const theme = useTheme()
   const { logoutUser, isLoggedIn } = useContext(UserContext) as UserContextType
+  const matches = useMediaQuery(theme.breakpoints.up('sm'))
+  const [open, setOpen] = useState(false)
+
+  const toggleDrawer = (newOpen: boolean) => () => {
+    setOpen(newOpen)
+  }
+
+  const HeaderOptions = ({ direction }: { direction: 'row' | 'column' }) => {
+    return (
+      <Stack direction={direction} spacing={2}>
+        <Button color="inherit" onClick={() => router.push('/')}>
+          Home
+        </Button>
+        <Button color="inherit" onClick={() => router.push('/dashboard')}>
+          Dashboard
+        </Button>
+        {isLoggedIn ? (
+          <Button color="inherit" onClick={() => logoutUser()}>
+            Logout
+          </Button>
+        ) : (
+          <Button color="inherit" onClick={() => router.push('/login')}>
+            Login
+          </Button>
+        )}
+      </Stack>
+    )
+  }
+
+  const DrawerButton = () => {
+    return (
+      <div>
+        <IconButton color="inherit" onClick={toggleDrawer(true)}>
+          <MenuIcon />
+        </IconButton>
+        <Drawer open={open} onClose={toggleDrawer(false)}>
+          <HeaderOptions direction="column" />
+        </Drawer>
+      </div>
+    )
+  }
+
   return (
-    <>
+    <Box>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -27,28 +76,12 @@ const NavBar = () => {
             <BugReport />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            BugTracker
+            <Link href="/">BugTracker</Link>
           </Typography>
-          <Stack direction="row" spacing={2}>
-            <Button color="inherit" onClick={() => router.push('/')}>
-              Home
-            </Button>
-            <Button color="inherit" onClick={() => router.push('/dashboard')}>
-              Dashboard
-            </Button>
-            {isLoggedIn ? (
-              <Button color="inherit" onClick={() => logoutUser()}>
-                Logout
-              </Button>
-            ) : (
-              <Button color="inherit" onClick={() => router.push('/login')}>
-                Login
-              </Button>
-            )}
-          </Stack>
+          {matches ? <HeaderOptions direction="row" /> : <DrawerButton />}
         </Toolbar>
       </AppBar>
-    </>
+    </Box>
   )
 }
 
