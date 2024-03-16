8238f6d13ae97b70b6786fbff4b6c55acd8703f6
R894 <kamil.khalaileh@gmail.com>
2024-03-16 04:49:02 +0200

    chore: add dummy text

--- client/src/components/FeaturesDisplay/FeaturesDisplay.tsx
+++ client/src/components/FeaturesDisplay/FeaturesDisplay.tsx
@@ -9,7 +9,8 @@ const Feature = ({ children }: { children: ReactNode }) => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        padding: '16px'
+        padding: '16px',
+        maxWidth:'240px'
       }}
     >
       {children}
@@ -23,24 +24,18 @@ const FeaturesDisplay = () => {
       <Stack direction={'row'} gap={3}>
         <Feature>
           <Apartment fontSize="large" />
-          <Typography variant='h5'>Feature 1</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
+          <Typography variant='h5'>Issue Creation</Typography>
+          <Typography textAlign={'center'}>Easily create new issues with detailed information.</Typography>
         </Feature>
         <Feature>
           <Apartment fontSize="large" />
-          <Typography variant='h5'>Feature 2</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
+          <Typography variant='h5'>Issue Tracking</Typography>
+          <Typography textAlign={'center'}>Keep track of the status, priority, and assignee for each issue.</Typography>
         </Feature>
         <Feature>
           <Apartment fontSize="large" />
-          <Typography variant='h5'>Feature 3</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
-          <Typography>Write description here</Typography>
+          <Typography variant='h5'>Safe</Typography>
+          <Typography textAlign={'center'}>Securely manage access with user authentication.</Typography>
         </Feature>
       </Stack>
     </>
