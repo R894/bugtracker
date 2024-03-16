d1b61367761aabc9dda0f537a7a411c99dfa7958
R894 <kamil.khalaileh@gmail.com>
2024-03-16 04:48:45 +0200

    fix: runtime error when no response

--- client/src/components/DashboardDisplay/DashboardDisplay.tsx
+++ client/src/components/DashboardDisplay/DashboardDisplay.tsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import BugList from './BugList'
 import ProjectsMenu from './ProjectsMenu'
-import { UserContext, UserContextType } from '@/context/UserContext'
+import { useUserContext } from '@/context/UserContext'
 import { getBugsByProjectId } from '@/api/bugService'
 import { Bug } from '@/api/models/bug'
 import CreateProjectModal from './CreateProjectModal'
@@ -9,9 +9,14 @@ import { Stack } from '@mui/material'
 import CreateBugModal from './CreateBugModal'
 
 const DashboardDisplay = () => {
-  const { selectedProject, setSelectedProject, updateUserProjects, user, token, logoutUser } = useContext(
-    UserContext,
-  ) as UserContextType
+  const {
+    selectedProject,
+    setSelectedProject,
+    updateUserProjects,
+    user,
+    token,
+    logoutUser,
+  } = useUserContext()
   const [bugs, setBugs] = useState<Bug[]>([])
 
   useEffect(() => {
@@ -23,7 +28,7 @@ const DashboardDisplay = () => {
     if (!token || selectedProject == null) return
     const getBugs = async () => {
       const response = await getBugsByProjectId(selectedProject.id, token)
-      if(response.code == 401) {
+      if (response && response.code == 401) {
         logoutUser()
         return
       }
