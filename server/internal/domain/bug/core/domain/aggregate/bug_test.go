dd97153225fd8c3d5802edfffab3c70f2009a77c
R894 <kamil.khalaileh@gmail.com>
2024-03-24 00:33:00 +0200

    test: add tests

--- server/internal/domain/bug/core/domain/aggregate/bug_test.go
+++ server/internal/domain/bug/core/domain/aggregate/bug_test.go
@@ -3,6 +3,8 @@ package aggregate
 import (
 	"testing"
 
+	"github.com/R894/bugtracker/internal/domain/bug/core/domain/entity"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,4 +38,64 @@ func TestUpdateDetails(t *testing.T) {
 	assert.NotEqual(t, bugReq.Description, bug.Description)
 	assert.NotZero(t, bug.CreatedAt)
 	assert.NotZero(t, bug.UpdatedAt)
+}
+
+func TestAssignTo(t *testing.T) {
+	bugReq := CreateBugRequest{
+		Title:       "test",
+		Description: "test",
+	}
+
+	bug := NewBug(bugReq)
+
+	assignee := "John Doe"
+	bug.AssignTo(assignee)
+
+	assert.Equal(t, assignee, bug.Assignee)
+	assert.NotZero(t, bug.UpdatedAt)
+}
+
+func TestChangeStatus(t *testing.T) {
+	bugReq := CreateBugRequest{
+		Title:       "test",
+		Description: "test",
+	}
+
+	bug := NewBug(bugReq)
+
+	status := entity.BugStatusClosed
+	bug.ChangeStatus(status)
+
+	assert.Equal(t, status, bug.Status)
+	assert.NotZero(t, bug.UpdatedAt)
+}
+
+func TestUpdateDetailsWithSameValues(t *testing.T) {
+	bugReq := CreateBugRequest{
+		Title:       "test",
+		Description: "test",
+	}
+
+	bug := NewBug(bugReq)
+
+	bug.UpdateDetails("test", "test")
+
+	assert.Equal(t, bugReq.Title, bug.Title)
+	assert.Equal(t, bugReq.Description, bug.Description)
+	assert.Equal(t, bug.CreatedAt, bug.UpdatedAt)
+}
+
+func TestUpdateDetailsWithEmptyValues(t *testing.T) {
+	bugReq := CreateBugRequest{
+		Title:       "test",
+		Description: "test",
+	}
+
+	bug := NewBug(bugReq)
+
+	bug.UpdateDetails("", "")
+
+	assert.Equal(t, bugReq.Title, bug.Title)
+	assert.Equal(t, bugReq.Description, bug.Description)
+	assert.Equal(t, bug.CreatedAt, bug.UpdatedAt)
 }
