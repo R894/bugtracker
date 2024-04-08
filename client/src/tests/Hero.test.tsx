e7d97feb55b81a00f957f0f71f7465b3782507ab
R894 <kamil.khalaileh@gmail.com>
2024-04-09 00:31:32 +0300

    fix: hero tests not having context

--- client/src/tests/Hero.test.tsx
+++ client/src/tests/Hero.test.tsx
@@ -2,15 +2,29 @@ import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import Hero from '@/components/Hero/Hero'
+import UserProvider from '@/context/UserContext'
+import ProjectProvider from '@/context/ProjectContext'
 
 test('renders BugTracker text', () => {
-  render(<Hero />)
+  render(
+    <UserProvider>
+      <ProjectProvider>
+        <Hero />
+      </ProjectProvider>
+    </UserProvider>,
+  )
   const bugTrackerText = screen.getByText('BugTracker')
   expect(bugTrackerText).toBeInTheDocument()
 })
 
 test('renders main message', () => {
-  render(<Hero />)
+  render(
+    <UserProvider>
+      <ProjectProvider>
+        <Hero />
+      </ProjectProvider>
+    </UserProvider>,
+  )
   const mainMessage = screen.getByText(
     'BugTracker centralizes communication, allowing teams to collaborate effortlessly and keep everyone on the same page.',
   )
