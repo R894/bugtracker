b9ad4619657d93ad735dd21f224f3c7e8087c3e3
R894 <kamil.khalaileh@gmail.com>
2024-04-14 00:21:51 +0300

    style: add responsive design to index page

--- client/src/components/FeaturesDisplay/FeaturesDisplay.tsx
+++ client/src/components/FeaturesDisplay/FeaturesDisplay.tsx
@@ -9,8 +9,10 @@ const Feature = ({ children }: { children: ReactNode }) => {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        justifyContent: 'center',
         padding: '16px',
         maxWidth: '240px',
+        minHeight: '240px',
       }}
     >
       {children}
@@ -20,31 +22,33 @@ const Feature = ({ children }: { children: ReactNode }) => {
 
 const FeaturesDisplay = () => {
   return (
-    <>
-      <Stack direction={'row'} gap={3}>
-        <Feature>
-          <NoteAdd fontSize='large'></NoteAdd>
-          <Typography variant="h5">Issue Creation</Typography>
-          <Typography textAlign={'center'}>
-            Easily create new issues with detailed information.
-          </Typography>
-        </Feature>
-        <Feature>
-          <Timeline fontSize="large"></Timeline>
-          <Typography variant="h5">Issue Tracking</Typography>
-          <Typography textAlign={'center'}>
-            Keep track of the status, priority, and assignee for each issue.
-          </Typography>
-        </Feature>
-        <Feature>
-          <Lock fontSize="large"></Lock>
-          <Typography variant="h5">Safety</Typography>
-          <Typography textAlign={'center'}>
-            Securely manage access with user authentication.
-          </Typography>
-        </Feature>
-      </Stack>
-    </>
+    <Stack
+      direction={'row'}
+      gap={3}
+      sx={{ flexWrap: 'wrap', justifyContent: 'center' }}
+    >
+      <Feature>
+        <NoteAdd fontSize="large"></NoteAdd>
+        <Typography variant="h5">Issue Creation</Typography>
+        <Typography textAlign={'center'}>
+          Easily create new issues with detailed information.
+        </Typography>
+      </Feature>
+      <Feature>
+        <Timeline fontSize="large"></Timeline>
+        <Typography variant="h5">Issue Tracking</Typography>
+        <Typography textAlign={'center'}>
+          Keep track of the status, priority, and assignee for each issue.
+        </Typography>
+      </Feature>
+      <Feature>
+        <Lock fontSize="large"></Lock>
+        <Typography variant="h5">Safety</Typography>
+        <Typography textAlign={'center'}>
+          Securely manage access with user authentication.
+        </Typography>
+      </Feature>
+    </Stack>
   )
 }
 
--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -1,10 +1,13 @@
 import { useUserContext } from '@/context/UserContext'
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { Variant } from '@mui/material/styles/createTypography'
 import { useRouter } from 'next/router'
 
 const Hero = () => {
   const router = useRouter()
   const { isLoggedIn } = useUserContext()
+  const theme = useTheme()
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'))
   return (
     <Box
       sx={{
@@ -18,8 +21,11 @@ const Hero = () => {
       maxWidth="sm"
       gap={4}
     >
-      <Typography variant="h1">BugTracker</Typography>
-      <Typography variant="h6" style={{ opacity: '80%', textAlign: 'center' }}>
+      <Typography variant={isSmallScreen ? 'h3' as Variant : 'h1' as Variant}>BugTracker</Typography>
+      <Typography
+        variant={isSmallScreen ? 'h7' as Variant : 'h6' as Variant } 
+        style={{ opacity: '80%', textAlign: 'center' }}
+      >
         BugTracker centralizes communication, allowing teams to collaborate
         effortlessly and keep everyone on the same page.
       </Typography>
