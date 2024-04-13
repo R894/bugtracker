b8c749bb81d2bfd9b12a8a7ca3fd861a2434e100
R894 <kamil.khalaileh@gmail.com>
2024-04-14 00:21:25 +0300

    chore: modify BugTable to use DataGrid

--- client/src/components/DashboardDisplay/BugTable.tsx
+++ client/src/components/DashboardDisplay/BugTable.tsx
@@ -1,57 +1,32 @@
 import { Bug } from '@/api/models/bug'
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@mui/material'
+import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import moment from 'moment'
 import Link from 'next/link'
 
-const BugRow = ({ bug }: { bug: Bug }) => {
-  return (
-    <TableRow
-      key={bug.id}
-      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-    >
-      <TableCell component="th" scope="row">
-        <b>
-          <Link href={`/issue/${bug.id}`}>{bug.title}</Link>
-        </b>
-      </TableCell>
-      <TableCell align="right">{bug.priority}</TableCell>
-      <TableCell align="right">{bug.status}</TableCell>
-      <TableCell align="right">{bug.asignee}</TableCell>
-      <TableCell align="right">
-        {moment(bug.createdAt).format('MMM Do YY')}
-      </TableCell>
-    </TableRow>
-  )
-}
+const columns: GridColDef[] = [
+  {
+    field: 'title',
+    headerName: 'Title',
+    width: 300,
+    renderCell: (params) => (
+      <Link href={`/issue/${params.row.id}`}>{params.row.title}</Link>
+    ),
+  },
+  { field: 'priority', headerName: 'priority' },
+  { field: 'status', headerName: 'Status' },
+  { field: 'asignee', headerName: 'Asignee' },
+  {
+    field: 'createdAt',
+    headerName: 'Date',
+    renderCell: (params) => {
+      return moment(params.row.createdAt).format('DD/MM/YYYY')
+    },
+  },
+]
 
 const BugTable = ({ bugs }: { bugs: Bug[] }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 1080 }} aria-label="Bug Table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Title</TableCell>
-            <TableCell align="right">Priority</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Assignee</TableCell>
-            <TableCell align="right">Date Created</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {bugs &&
-            bugs.length >= 1 &&
-            bugs.map((row) => <BugRow key={row.id} bug={row} />)}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <DataGrid rows={bugs || []} columns={columns} autoHeight />
   )
 }
 
